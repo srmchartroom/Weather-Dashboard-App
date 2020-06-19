40957f95dc945351b57c949c1f5c56109077b8fe
srmchartroom <sean@chartroomcreative.com>
2020-06-19 03:10:20 -0400

    Created skeleton file structure with html, script, styles, and appropriate links between files.

---
+++ script.js
@@ -0,0 +1,2 @@
+//? This file handles all scripting functionality of the Weather Dashboard Application.
+//?  See specific comments within the below for direction on functionality for your own refactoring and use.
