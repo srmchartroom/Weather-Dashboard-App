e0b8733d68041d08b10f04bb1400d26f192ff8bf
srmchartroom <sean@chartroomcreative.com>
2020-06-22 13:01:12 -0400

    Added new variables to handle current forecast data, created dynamic handling of UV index coloration. Set tooltips that dynamically change with UV levels, and with current weather conditions. Set dynamic variables to create new date objects based on API's returned values and updated dom for all current weather information for a given city.

--- script.js
+++ script.js
@@ -1,109 +1,135 @@
 //? This file handles all scripting functionality of the Weather Dashboard Application.
 //?  See specific comments within the below for direction on functionality for your own refactoring and use.
 
-//! --- API VARIABLES --- //
-
-let currCity = ""; // variable to be updated with searched city or selection of a previous city
-let fullCity = "q=" + currCity;
-let currZip = "27103"; // variable to be updated with search zip or selection of a previous city
-let fullZip = ("zip=" + currZip).trim();
-let currLocation = fullZip;
-let currIcon = "";
-
-//! --- NOTE TO DEVELOPERS & CONTRIBUTORS: Please be sure to secure your own free API key from api.openweathermap.org, and replace the value for the "apiKey" variable below --- //
-const apiKey = "23f82e4cdf1dd3340d14e89dd9f184b8"; // API key with openWeather  
-
-let queryURL1 = "https://api.openweathermap.org/data/2.5/weather?" + currLocation + "&appid=" + apiKey + "&units=imperial"; // add "&units=imperial" so Kelvin:Farenheit conversion isn't necessary
-let currLon = ""; // will be "r1.coord.lon"; // current longitude
-let finalLon = "lon=" + currLon;
-let currLat = ""; // will be "r1.coord.lat"; // current latitude
-let finalLat = "lat=" + currLat;
-let queryURL2 = ""; //"https://api.openweathermap.org/data/2.5/onecall?" + finalLat + "&" + finalLon + "exclude=hourly,daily&appid=" + apiKey;""
-
-$.ajax({ // <!-- ! AJAX API call for OpenWeather's api to get lat and lon of current city
-  url: queryURL1,
-  method: "GET"
-  }).then(function(response1) {  
-  console.log(response1);
-  currLon = response1.coord.lon;
-  currLat = response1.coord.lat;
-  currCity = response1.name;
-  currIcon = response1.weather[0].icon;
-  console.log(currIcon);
-  let currIconImg = '<img src="http://openweathermap.org/img/wn/' + currIcon + '@2x.png" />';
-  queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currLat + "&lon=" + currLon + "&exclude=hourly,minutely&appid=" + apiKey + "&units=imperial";
-  let currDate = new Date(response1.dt * 1000).toLocaleDateString();
-  $("#currDate").html(currDate);
-  $("#currCity").html(currCity);
-  $("#currIcon").html(currIconImg);
-
-  let currDay = new Date(response1.dt * 1000).getDay();
-  console.log(currDay);
-  // let currMonth = (currDate.getMonth()) -1;
-  // console.log(currMonth);
-  // let currMDate = currDate.getDate();
-  // console.log(currMDate);
-  // let currYear = currDate.getFullYear();
-  // console.log(currYear);
-  // let combinedDate = currMonth + "/" + currMDate + "/" + currYear;
-  // console.log(combinedDate);
-  $.ajax({
-    url: queryURL2,
-    method: "GET"
-    }).then(function(responseB) {
-   console.log(responseB);
-  });
-  // let currDate = response1.dt;
-  // let milliseconds = currDate * 1000;
-  // let dateObject = new Date(milliseconds);
-  // let currDate = new Date(response1.dt * 1000).toLocaleString();
-  // currDate.getDay();
-  // console.log(currDate);
-  // let currMonth = (currDate.getMonth()) -1;
-  // console.log(currMonth);
-  // let currMDate = currDate.getDate();
-  // console.log(currMDate);
-  // let currYear = currDate.getFullYear();
-  // console.log(currYear);
-  // let combinedDate = currMonth + "/" + currMDate + "/" + currYear;
-  // console.log(combinedDate);
-});
-// queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currLat + "&lon=" + currLon + "exclude=hourly,daily&appid=" + apiKey;
-  // console.log(queryURL2);
-  
-  
-
-let currTemp = "r2.current.temp"; // current temperature
-let currHumidity = "r2.current.humidity"; // current humidity
-let currWind = "r2.current.wind_speed"; // current wind speed
-let currUVI = "r2.current.uvi"; // current UV index
-
-let d1TempMin = "response.daily.temp.min"; // ?? ?? 
-
-// let queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?" + finalLat + "&" + finalLon + "exclude=hourly,daily&appid=" + apiKey;
-
-
 //! --- UV TOOLTIPS --- //
 
 // Enabling Tooltips for UV indexes
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
 // Setting variables to hold the tooltip text. This will be placed in the "title" attribute of the #currUVspan based on the current selected city's UV Index.
 
-const ttLow = "LOW: 1-2 UVI | Safe to be outdoors - No protection required.";
-const ttMod = "Moderate: UV Index of 3-5 | Take precautions, such as covering up during midday hours, wearing a hat and sunglasses and using sunscreen. Stay in the shade near midday when the sun is strongest."; 
-const ttHigh = "High: 6-7 UVI | Wear sunglasses and use SPF 30+ sunscreen, cover the body with sun protective clothing and a wide-brim hat, and reduce time in the sun from three hours before to three hours after solar noon.";
-const ttVHigh = "Very High: 8-10 UVI | Wear SPF 30+ sunscreen, a shirt, sunglasses, and a hat. Do not stay out in the sun for too long.";
-const ttExtreme = "Moderate: 11+ UVI | Wear SPF 30+ sunscreen, a shirt, sunglasses, and a hat. Do not stay out in the sun for too long.";
+const ttLow = 'LOW (1-2 UVI): Safe to be outdoors - No protection required.';
+const ttMod = 'Moderate (3-5 UVI):  Take precautions, such as covering up during midday hours, wearing a hat and sunglasses and using sunscreen. Stay in the shade near midday when the sun is strongest.'; 
+const ttHigh = 'High (6-7 UVI):  Wear sunglasses and use SPF 30+ sunscreen, cover the body with sun protective clothing and a wide-brim hat, and reduce time in the sun from three hours before to three hours after solar noon.';
+const ttVHigh = 'Very High (8-10 UVI):  Wear SPF 30+ sunscreen, a shirt, sunglasses, and a hat. Do not stay out in the sun for too long.';
+const ttExtreme = 'Extreme (11+ UVI):  Wear SPF 30+ sunscreen, a shirt, sunglasses, and a hat. Do not stay out in the sun for too long.';
 
 // Setting easy variables for handling the UV block's background class
-const uvClassLow = "bg-low";
-const uvClassMod = "bg-moderate";
-const uvClassHigh = "bg-high";
-const uvClassVHigh = "bg-vhigh";
-const uvClassExtreme = "bg-extreme";
+const uvClassLow = "btn bg-low";
+const uvClassMod = "btn bg-moderate";
+const uvClassHigh = "btn bg-high";
+const uvClassVHigh = "btn bg-vhigh";
+const uvClassExtreme = "btn bg-extreme";
+
+//! --- API VARIABLES --- //
+
+//! --- NOTE TO DEVELOPERS & CONTRIBUTORS: Please be sure to secure your own free API key from api.openweathermap.org, and replace the value for the "apiKey" variable below --- //
+const apiKey = "23f82e4cdf1dd3340d14e89dd9f184b8"; // API key with openWeather  
+
+// API VARS TO POPULATE FROM OR USE WITH WEATHER API OF OPENWEATHERMAP.ORG //
+let currCity = ""; // variable to be updated with searched city or selection of a previous city
+let fullCity = "q=" + currCity; // city query string param to use in the weather API of OpenWeatherMap.org
+let currZip = "27103"; // variable to be updated with search zip or selection of a previous city
+let fullZip = ("zip=" + currZip).trim(); // zip query string param to use in the weather API of Open Weather
+let currLocation = fullZip; // location query string param to use in the weather API of OpenWeatherMap.org
+let currIcon = ""; // current selection's weather icon for today
+let currCondition = ""; // current selection's condition description for today
+let queryURL1 = "https://api.openweathermap.org/data/2.5/weather?" + currLocation + "&appid=" + apiKey + "&units=imperial"; // URL for initial weather AJAX call; includes "&units=imperial" so Kelvin:Farenheit conversion isn't necessary;
+let currDate = ""; // current date
+let currLon = ""; // current selection's longitude to use in params for oneCall API of OpenWeatherMap.org
+let currLat = ""; // current selection's latitude to use in paarams for oneCall API of OpenWeatherMap.org
+
+// API VARS TO POPULATE FROM OR USE WITH ONECALL API OF OPENWEATHERMAP.ORG //
+let currTemp = ""; // current selection's Temp for today from oneCall API of OpenWeatherMap.org
+let currHumidity = ""; // current selection's Humidity for today from oneCall API of OpenWeatherMap.org
+let currWind = ""; // current selection's Wind Speed for today from oneCall API of OpenWeatherMap.org
+let currUVI = "" ; // current selection's UVI for today from oneCall API of OpenWeatherMap.org
+let currUVIicon = ' <i class="fas fa-info-circle fa-x"></i>';
+
+//! --- AJAX CALL: WEATHER API OF OPENWEATHERMAP.ORG --- //
+
+$.ajax({ // AJAX API call for OpenWeather's api to get lat and lon of current city
+  url: queryURL1, // Sets the URL argument equal to the "queryURL1" var 
+  method: "GET" // Sets the method argument equal to "GET"
+  }).then(function(responseOne) {  // Sets anonymous function to run once the GET request completes
+    console.log(responseOne); // TODO Remove this once no longer needed
+    currLon = responseOne.coord.lon; // sets response's longitude value to "currLon" var
+    currLat = responseOne.coord.lat; // sets response's latitude value to "currLat" var
+    currCity = responseOne.name; // sets response's name value to "currCity" var
+    $("#currCity").html(currCity); // Updates the #currCity span's HTML with the currCity var's value
+    currIcon = responseOne.weather[0].icon; // sets response's icon value to "currIcon" var
+    $("#currIcon").html('<img src="http://openweathermap.org/img/wn/' + currIcon + '@2x.png" style="margin-right: -20px;"/><span><i class="fas fa-info-circle pl-0 ml-0 mb-4 super align-bottom" style="font-size: small;"></i></span>'); // updates #currIcon span's HTML with an <img> tag leveraging the currIcon var which is set based on the response
+    let currDate = new Date(responseOne.dt * 1000).toLocaleDateString(); // creates a new date object, converts it to milliseconds, and then converts it to the short MM/DD/YYYY format
+    $("#currDate").html(currDate); // updates the #currDate span's HTML with the currDate var and converted date object value
+    console.log(currLon);
+    console.log(currLat);
+    
+    //! --- AJAX CALL: ONECALL API OF OPENWEATHERMAP.ORG --- //
+    let queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currLat + "&lon=" + currLon + "&exclude=hourly,minutely&appid=" + apiKey + "&units=imperial";
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+      }).then(function(responseTwo) {
+        console.log(responseTwo);
+
+        currTemp = responseTwo.current.temp; // current temperature
+        console.log(currTemp);
+        $("").attr("data-original-title", currCondition);
+        $("#currTemp").html(currTemp + " &deg;F");
+        currHumidity = responseTwo.current.humidity; // current humidity
+        console.log(currHumidity);
+        $("#currHumidity").html(currHumidity + "%");
+        currWind = responseTwo.current.wind_speed; // current wind speed
+        console.log(currWind);
+        $("#currWind").html(currWind + " MPH");
+        currUVI = responseTwo.current.uvi; // current UV index
+        console.log(currUVI);
+        $("#currUVbtn").html(currUVI + currUVIicon);
+        if(currUVI < 3) {
+          console.log("Low UVI:" + currUVI);
+          $("#currUVspan").attr("data-original-title", ttLow);
+          $("#currUVbtn").removeClass("bg-low","bg-moderate","bg-high","bg-vhigh","bg-extreme");
+          $("#currUVbtn").addClass("bg-low");
+        } else if (currUVI >= 3 && currUVI < 6) {
+          console.log("Moderate UVI:" + currUVI);
+          $("#currUVspan").attr("data-original-title",ttMod);
+          $("#currUVbtn").removeClass("bg-low","bg-moderate","bg-high","bg-vhigh","bg-extreme");
+          $("#currUVbtn").addClass("bg-moderate");
+        } else if (currUVI >= 6 && currUVI < 8) {
+          console.log("High UVI:" + currUVI);
+          $("#currUVspan").attr("data-original-title",ttHigh);
+          $("#currUVbtn").removeClass("bg-low","bg-moderate","bg-high","bg-vhigh","bg-extreme");
+          $("#currUVbtn").addClass("bg-high");
+        } else if (currUVI >= 8 && currUVI < 11) {
+          console.log("Very High UVI:" + currUVI);
+          $("#currUVspan").attr("data-original-title", ttVHigh);
+          $("#currUVbtn").removeClass("bg-low","bg-moderate","bg-high","bg-vhigh","bg-extreme");
+          $("#currUVbtn").addClass("bg-vhigh");
+        } else if (currUVI >= 11) {
+          console.log("Extreme UVI:" + currUVI);
+          $("#currUVspan").attr("data-original-title",ttExtreme);
+          $("currUVbtn").removeClass("bg-low","bg-moderate","bg-high","bg-vhigh","bg-extreme");
+          $("#currUVbtn").addClass("bg-extreme");
+        }
+        let currConditionMain = responseTwo.current.weather[0].main; // sets response's condition value to "currCondition" var
+        let currConditionDescrip = responseTwo.current.weather[0].description; // sets 
+        currCondition = currConditionMain + ": " + currConditionDescrip;
+        $("#currIcon").attr("data-original-title", currCondition);
+
+        let currDay = new Date(responseTwo.current.dt * 1000).getDay();
+        console.log(currDay);
+    });
+
+});
+
+
+
+
+// queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currLat + "&lon=" + currLon + "&exclude=hourly,minutely&appid=" + apiKey + "&units=imperial";
+  
+
 
 //-----------------------------------------------------------------------------------------
 
